5c1a382ac502bc6e8639ba9210a76601d251c685
Radek <dolorismachina:gmail.com>
2018-10-06 19:27:56 +0100

    Removed unused functions.

--- taskmanager.js
+++ taskmanager.js
@@ -1,15 +1,4 @@
 export default {
-  collapse: function(task, element, parent) {
-    element.classList.toggle('collapsed');
-  },
-
-  remove: function(task, parent, element) {
-    const id = parent.subTasks.indexOf(task);
-    parent.subTasks.splice(id, 1);
-  
-    element.remove();
-  },
-
   add: function(task, parent, element, taskElement) {
     if (!parent.hasOwnProperty('subTasks')) {
       parent.subTasks = [];
